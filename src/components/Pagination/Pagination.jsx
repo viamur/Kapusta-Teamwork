0b0fa8e31f91e9e989254cfe9517bb7974c7922e
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-08-24 15:01:33 +0300

    node-sass  delete

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,12 @@
+import s from './Pagination.module.scss';
+const Pagination=()=>{
+
+    return(
+        <div>
+            <p className={s.paragraph}>Current period:</p>
+            <button type="button"></button>
+
+            <button type="button"></button>
+        </div>
+    
+)}
