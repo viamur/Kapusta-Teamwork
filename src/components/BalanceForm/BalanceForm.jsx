de31bf96e221b295d9794fad2e7d0510e54371ce
Moskalenko Serhii <mynameserhii@gmail.com>
2022-08-25 21:52:26 +0300

    25

--- src/components/BalanceForm/BalanceForm.jsx
+++ src/components/BalanceForm/BalanceForm.jsx
@@ -2,22 +2,34 @@ import s from './BalanceForm.module.scss';
 import { useFormik } from 'formik';
 import NumberFormat from 'react-number-format';
 import BalanceModal from 'components/BalanceModal/BalanceModal';
+import { useSelector } from 'react-redux';
+import { getAuthBalance } from 'redux/auth/AuthSelectors';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const BalanceForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      balance: '',
-    },
-    onSubmit: values => {
-      console.log(values.balance);
-    },
-  });
+  const [balance, setBalance] = useState(0);
+
+  const getBalance = useSelector(getAuthBalance);
+  useEffect(() => {
+    if (balance) return;
+    setBalance(getBalance);
+  }, [getBalance]);
+
+  const handleChange = e => {
+    /* ==========нужно обрезать значение с инпута и перевести в число!!!============= */
+    setBalance(e.target.value);
+  };
+
+  const handleSubmit = e => {};
+
   return (
     <div className={s.balance}>
       <p className={s.title}>Balance:</p>
-      <form onSubmit={formik.handleSubmit} className={s.form}>
+      <form onSubmit={handleSubmit} className={s.form}>
         <NumberFormat
           className={s.input}
+          disabled={getBalance > 0}
           name="balance"
           suffix={' UAH'}
           allowLeadingZeros={true}
@@ -26,15 +38,15 @@ const BalanceForm = () => {
           placeholder={'00.00 UAH'}
           fixedDecimalScale={true}
           allowNegative={false}
-          onChange={formik.handleChange}
-          value={formik.values.balance}
+          onChange={handleChange}
+          value={balance}
         />
-        <button type="submit" className={s.submit} disabled={formik.values.balance.length === 0}>
+        <button type="submit" className={s.submit} disabled={getBalance > 0}>
           Confirm
         </button>
       </form>
       {/* временно сделал так */}
-      {formik.values.balance.length === 1 && <BalanceModal />}
+      {balance === 0 && <BalanceModal />}
     </div>
   );
 };
