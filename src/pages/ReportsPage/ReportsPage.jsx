e2d228fdfa85cd4cb14469cc71131ee166b57d20
Oleksii Chyharov <chyharov@gmail.com>
2022-08-26 23:47:33 +0300

    buckup for night

--- src/pages/ReportsPage/ReportsPage.jsx
+++ src/pages/ReportsPage/ReportsPage.jsx
@@ -2,7 +2,7 @@ import Balance from 'components/Balance/Balance';
 import Reports from 'components/Reports/Reports';
 import CategoryList from '../../components/CategoryList/CategoryList';
 // import ChartCategory from '../../components/ChartCategory/ChartCategory';
-// import ChartCategoryMobile from '../../components/ChartCategoryMobile/ChartCategoryMobile';
+import ChartCategoryMobile from '../../components/ChartCategoryMobile/ChartCategoryMobile';
 
 const ReportsPage = () => {
   return (
@@ -11,7 +11,7 @@ const ReportsPage = () => {
       <Reports />
       <CategoryList />
       {/* <ChartCategory /> */}
-      {/* <ChartCategoryMobile /> */}
+      <ChartCategoryMobile />
     </>
   );
 };
