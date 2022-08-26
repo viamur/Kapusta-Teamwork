1ce731a65e64a3ace64e0f68054521aaea32ae1c
Oleksii Chyharov <chyharov@gmail.com>
2022-08-26 16:56:56 +0300

    save for merge

---
+++ src/components/BalanceForm/BalanceForm.jsx
@@ -0,0 +1,74 @@
+import s from './BalanceForm.module.scss';
+import NumberFormat from 'react-number-format';
+import BalanceModal from 'components/BalanceModal/BalanceModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
+import { getAuthBalance } from 'redux/auth/AuthSelectors';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { newBalance } from 'redux/auth/authOperations';
+// import { useLocation } from 'react-router-dom';
+
+const BalanceForm = ({ isReportsPage }) => {
+  const [balance, setBalance] = useState();
+
+  const dispatch = useDispatch();
+  const mob = useMediaQuery({ query: '(max-width: 768px)' });
+  const desk = useMediaQuery({ query: '(min-width: 1280px)' });
+
+  const getBalance = useSelector(getAuthBalance);
+  useEffect(() => {
+    if (getBalance === 0 || balance === getBalance) return;
+    setBalance(getBalance);
+  }, [getBalance]);
+
+  const handleChange = e => {
+    const value = e.target.value;
+    const sliceValue = value
+      .slice(0, value.length - 4)
+      .split(' ')
+      .join('');
+    const mathRound = Math.round(sliceValue);
+    setBalance(mathRound);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    dispatch(newBalance({ newBalance: balance }));
+  };
+  return (
+    <div className={isReportsPage ? s.balanceReports : s.balance}>
+      <p className={s.title}>Balance:</p>
+      <form onSubmit={handleSubmit} className={s.form}>
+        <NumberFormat
+          className={isReportsPage ? s.inputRepots : s.input}
+          disabled={getBalance > 0}
+          name="balance"
+          suffix={' UAH'}
+          allowLeadingZeros={true}
+          thousandSeparator={' '}
+          decimalScale={2}
+          placeholder={'00.00 UAH'}
+          fixedDecimalScale={true}
+          allowNegative={false}
+          onChange={handleChange}
+          value={balance}
+        />
+        {isReportsPage && desk && (
+          <button type="submit" className={s.submit} disabled={getBalance > 0}>
+            Confirm
+          </button>
+        )}
+        {!isReportsPage && (
+          <button type="submit" className={s.submit} disabled={getBalance > 0}>
+            Confirm
+          </button>
+        )}
+      </form>
+      {getBalance === 0 && <BalanceModal />}
+    </div>
+  );
+};
+
+export default BalanceForm;
