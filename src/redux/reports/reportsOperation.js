24cbc4a9d15bbb884cdc3d03ae8cd12876361544
Moskalenko Serhii <mynameserhii@gmail.com>
2022-08-27 16:01:32 +0300

    fix

--- src/redux/reports/reportsOperation.js
+++ src/redux/reports/reportsOperation.js
@@ -2,12 +2,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getPeriodTransactions } from 'utils/api';
 
 export const getPeriod = createAsyncThunk(
-  'transactions/getPeriodTransactions',
+  'reports/getPeriodTransactions',
   async (date, { rejectWithValue }) => {
-    console.log('gdgdhfh',date);
+    console.log('gdgdhfh', date);
     try {
       const { data } = await getPeriodTransactions(date);
-      console.log('dhfh',data);
+      console.log('dhfh', data);
       return data;
     } catch (err) {
       return rejectWithValue(err);
