87f918588e2adab44a323e44eaec096a06aad130
AlekseyDemydov <agdemidof@gmail.com>
2022-08-28 19:18:42 +0300

    fix

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import NumberFormat from 'react-number-format';
+import { useParams } from 'react-router-dom';
+
 import data from './month.json';
 import s from './Summary.module.scss';
 
 import * as selectors from '../../redux/transactions/transactionsSelector';
+import { useState } from 'react';
 
-const Summary = ({ data }) => {
+const Summary = () => {
+  const { transType } = useParams();
+  const [data, setData] = useState([]);
+  const pageExpenses = transType === 'expenses';
+  const pageIncome = transType === 'income';
+
   const incomes = useSelector(selectors.getTransactionsMonthsIncomes);
   const expenses = useSelector(selectors.getTransactionsMonthsExpenses);
 
-  const expensesArr = Object.entries(expenses);
-  //   const incomesArr = Object.entries(incomes);
-
+  useEffect(() => {
+    if (pageExpenses && expenses) {
+      const expensesArr = Object.entries(expenses);
+      setData(expensesArr);
+      return;
+    }
+    if (pageIncome && incomes) {
+      const incomesArr = Object.entries(incomes);
+      setData(incomesArr);
+      return;
+    }
+  }, [pageExpenses, pageIncome, incomes, expenses]);
   return (
     <div className={s.summaryContainer}>
       <h4 className={s.summaryTitle}>Summary</h4>
-      <ul>
-        {expensesArr
-          .filter(el => el[1] !== 'N/A')
-          .map((el, index) => (
+      <ul className={s.summaryList}>
+        {data
+          .filter(([test1, test2]) => test2 !== 'N/A')
+          .map(([test1, test2], index) => (
             <li key={index} className={s.summaryItem}>
-              <p>{el[0]}</p>
-              <NumberFormat
-                className={s.summarySumm}
-                value={el[1]}
-                suffix={' UAH'}
-                allowLeadingZeros={true}
-                thousandSeparator={' '}
-                decimalScale={2}
-                placeholder={'00.00 UAH'}
-                fixedDecimalScale={true}
-                allowNegative={false}
-              />
-              {/* <p className={s.summarySumm}>{el[1]}</p> */}
+              <p>{test1}</p>
+              <p className={s.summarySumm}>{test2} .00 uah</p>
             </li>
           ))}
       </ul>
