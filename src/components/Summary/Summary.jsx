bee3da38d563f55d5bb6ab94cc9a787bf8ed8c73
AlekseyDemydov <agdemidof@gmail.com>
2022-08-29 09:50:33 +0300

    Update Summary.jsx

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import translateMonths from './month';
-import num from './zero.json';
 import { useParams } from 'react-router-dom';
 
 import s from './Summary.module.scss';
