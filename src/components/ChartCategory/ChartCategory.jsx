064077d743b70d7847e43b2270091805f8f42f48
Moskalenko Serhii <mynameserhii@gmail.com>
2022-08-29 18:30:11 +0300

    st

--- src/components/ChartCategory/ChartCategory.jsx
+++ src/components/ChartCategory/ChartCategory.jsx
@@ -9,20 +9,20 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { Bar } from 'react-chartjs-2';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(CategoryScale, ChartDataLabels, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export const options = {
   indexAxis: 'x',
   responsive: true,
+  layout: {
+    /* Паддинг что б упустить чуть графики что б влезло сверху лейбел */
+    padding: {
+      top: 30,
+    },
+  },
   plugins: {
     legend: {
       display: false,
@@ -30,8 +30,19 @@ export const options = {
     title: {
       display: false,
     },
+    /* Стилизация лейбла позиция и тд */
+    datalabels: {
+      color: '#52555F',
+      anchor: 'end',
+      align: 'end',
+      offset: 5,
+      font: {
+        size: '12',
+      },
+      /* Формат лейбла*/
+      formatter: value => `${value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} UAH`,
+    },
   },
-
   minBarThickness: 15,
   maxBarThickness: 20,
 
@@ -45,26 +56,31 @@ export const options = {
       },
     },
     y: {
+      ticks: {
+        /* Убираем шкалу с лева */
+        display: false,
+      },
       grid: {
         color: '#F5F6FB',
         borderColor: 'transparent',
         tickColor: 'transparent',
-        lineWidth: 2,
       },
     },
   },
 };
 
-const labels = ['Transport'];
+const labels = ['Transport', 'bike', 'Transport', 'bike', 'Transport', 'bike'];
 
 export const data = {
   labels,
   datasets: [
     {
       label: '',
-      data: labels.map(() => 5000),
+      data: [10000, 3000, 5000, 1000, 8000, 3000],
       backgroundColor: ['#FF751D'],
       borderRadius: 10,
+      /* Ширина баров */
+      maxBarThickness: 38,
     },
   ],
 };
